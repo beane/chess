9923f0eebfca74521fe0f32209a3e008d1dead95
Brian Deane and B. Adam Russell <brian+adam@appacademy.io>
2013-10-30 13:55:27 -0400

    Abstracted Players classes and refactored Error Handling

--- game.rb
+++ game.rb
@@ -1,83 +1,103 @@
 require_relative "chessboard"
 require_relative "errors"
 require 'debugger'
-class Game
-  COLUMNS = { "a" => 0,
-              "b" => 1,
-              "c" => 2,
-              "d" => 3,
-              "e" => 4,
-              "f" => 5,
-              "g" => 6,
-              "h" => 7 }
+require_relative "players"
 
-  attr_reader :chess_board
+class Game
+
+
+  attr_reader :chess_board, :white_player, :black_player
 
   def initialize
     @chess_board = ChessBoard.new
-    @current_player = :white
+    @white_player = choose_player(:white)
+    @black_player = choose_player(:black)
+    @current_player = self.white_player
     chess_board.start_game
   end
 
-  def format_position(position)
-    col_letter = position[0]
-    row = position[1]
-
-    #either of the digits could be a letter
-    if ("a".."h") === position[1]
-      #second digit is the letter
-      col_letter = position[1]
-      row = position[0]
-    end
-
-    raise InvalidPositionError unless ("a".."g") === col_letter
-
-    col = COLUMNS[col_letter]
-    row = row.to_i-1 # Integer(row) can throw an error that we can rescue
-
-    raise InvalidPositionError unless (0...8) === row
-
-    [row, col]
+  def choose_player(color)
+    printf "Who plays #{color}? (h/c): "
+    player = gets.chomp.downcase
+    player == 'h' ? HumanPlayer.new(color) : Player.new(color)
   end
+
+  # put in human
+  # def format_position(position)
+  #   col_letter = position[0]
+  #   row = position[1]
+  #
+  #   #either of the digits could be a letter
+  #   if ("a".."h") === position[1]
+  #     #second digit is the letter
+  #     col_letter = position[1]
+  #     row = position[0]
+  #   end
+  #
+  #   raise InvalidPositionError unless ("a".."g") === col_letter
+  #
+  #   col = COLUMNS[col_letter]
+  #   row = row.to_i-1 # Integer(row) can throw an error that we can rescue
+  #
+  #   raise InvalidPositionError unless (0...8) === row
+  #
+  #   [row, col]
+  # end
 
   def wrong_piece(position)
-    chess_board[position].color != current_player
+    self.chess_board[position].color != self.current_player.color
   end
 
-  def prompt_user
-    begin
-      printf "Type in your move, #{self.current_player} (start,end): "
-      str_input = gets.downcase.chomp.split(",")
-      raise InvalidPositionError if str_input.size != 2
-      row1, col1 = str_input[0].split('')
-      row2, col2 = str_input[1].split('')
-
-      #raise error if str_input is not 2 digits
-      start = format_position([row1, col1])
-      final = format_position([row2, col2])
-
-      raise WrongPieceError if chess_board[start] && wrong_piece(start)
-
-      system("clear") # might want move around (passing stuff to errors?)
-
-      [start, final]
-    rescue InvalidPositionError => e
-      p e.message()
-      retry
-    rescue WrongPieceError => e
-      p e.message(current_player)
-      retry
-    end
-  end
+  # def prompt_user
+#     begin
+#       printf "Type in your move, #{self.current_player.color} (start,end): "
+#       str_input = gets.downcase.chomp.split(",")
+#       raise InvalidPositionError if str_input.size != 2
+#       row1, col1 = str_input[0].split('')
+#       row2, col2 = str_input[1].split('')
+#
+#       #raise error if str_input is not 2 digits
+#       start = format_position([row1, col1])
+#       final = format_position([row2, col2])
+#
+#       raise WrongPieceError if chess_board[start] && wrong_piece(start)
+#
+#       system("clear") # might want move around (passing stuff to errors?)
+#
+#       [start, final]
+#     rescue InvalidPositionError => e
+#       puts e.message()
+#       retry
+#     rescue WrongPieceError => e
+#       puts e.message(current_player.color)
+#       retry
+#     end
+#  end
 
   def game_turn
-    puts "CHECKED!" if self.chess_board.checked?(current_player)
+    begin
+      puts "CHECKED!" if self.chess_board.checked?(current_player.color)
 
-    puts self.chess_board
-    start, final = prompt_user
+      puts self.chess_board
+      start, final = self.current_player.get_move
+
+      raise WrongPieceError if chess_board[start] && wrong_piece(start)
+    rescue WrongPieceError => e
+      puts e.message(current_player.color)
+      retry
+    end
 
     self.chess_board.move(start, final)
   end
+
+  def change_player
+    if current_player.color == :black
+      self.current_player = white_player
+    else
+      self.current_player = black_player
+    end
+  end
+
 
   def play_game
 
@@ -88,19 +108,20 @@ class Game
       # ALL OF THESE ERRORS SHOULD COME FROM THE BOARD
       # RAISE MOVE INTO CHECK ERROR
       rescue MoveIntoCheckError => e
-        p e.message
+        puts e.message
         retry
       rescue EndPositionError => e
-        p e.message
+        puts e.message
         retry
       rescue StartPositionError => e
-        p e.message
+        puts e.message
         retry
       end
 
-      self.current_player = (current_player == :white ? :black : :white)
-      break if chess_board.checkmate?(current_player)
-      break if chess_board.draw?(current_player)
+      self.change_player
+
+      break if chess_board.checkmate?(current_player.color)
+      break if chess_board.draw?(current_player.color)
     end
 
   end
---
+++ players.rb
@@ -0,0 +1,70 @@
+class Player
+  attr_reader :color
+
+  def initialize(color)
+    @color = color
+  end
+end
+
+class HumanPlayer < Player
+  COLUMNS = { "a" => 0,
+              "b" => 1,
+              "c" => 2,
+              "d" => 3,
+              "e" => 4,
+              "f" => 5,
+              "g" => 6,
+              "h" => 7 }
+
+  def format_position(position)
+    col_letter = position[0]
+    row = position[1]
+
+    #either of the digits could be a letter
+    if ("a".."h") === position[1]
+      #second digit is the letter
+      col_letter = position[1]
+      row = position[0]
+    end
+
+    raise InvalidPositionError unless ("a".."g") === col_letter
+
+    col = COLUMNS[col_letter]
+    row = row.to_i-1 # Integer(row) can throw an error that we can rescue
+
+    raise InvalidPositionError unless (0...8) === row
+
+    [row, col]
+  end
+
+  def get_move
+    begin
+      printf "Type in your move, #{self.color} (start,end): "
+      str_input = gets.downcase.chomp.split(",")
+
+      raise InvalidPositionError if str_input.size != 2
+      row1, col1 = str_input[0].split('')
+      row2, col2 = str_input[1].split('')
+
+      #raise error if str_input is not 2 digits
+      start = format_position([row1, col1])
+      final = format_position([row2, col2])
+
+      #raise WrongPieceError if chess_board[start] && wrong_piece(start)
+
+      system("clear") # might want move around (passing stuff to errors?)
+
+      [start, final]
+    rescue InvalidPositionError => e
+      puts e.message()
+      retry
+    rescue WrongPieceError => e
+      puts e.message(current_player)
+      retry
+    end
+  end
+end
+
+class ComputerPlayer < Player
+
+end
